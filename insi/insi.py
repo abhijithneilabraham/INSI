792a31b5e6264475f59f88506fbd05da5dea2354
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2020-09-18 23:44:29 +0530

    tableqa for data querying

--- insi/insi.py
+++ insi/insi.py
@@ -1,6 +1,9 @@
 from pipelines import pipeline
 from utils import score_questions
+from tableqa.agent import Agent
+import os
 qg = pipeline("e2e-qg")
+
 
 class insi:
     def get_scores(self,questions):
@@ -12,6 +15,15 @@ class insi:
         qmaps=self.get_scores(questions)
         picked_questions=[k for k,v in qmaps.items() if v>0.9]
         return picked_questions
+    
+    def get_results(self,text,csv_path,schema=None):
+        questions=self.get_questions(text)
+        agent=Agent(csv_path,schema)
+        for q in questions:
+            res=agent.query_db(q)
+            print("Question:{}, Result :{}".format(q,res))
+        
+        
     
         
         

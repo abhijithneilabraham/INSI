ff8c9fba9954f761ff67981b0ef448391e819817
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2020-09-20 17:13:39 +0530

    updated qa if csv not given

--- insi/insi.py
+++ insi/insi.py
@@ -1,6 +1,7 @@
 from pipelines import pipeline
 from utils import score_questions
 from tableqa.agent import Agent
+from tableqa.nlp import qa
 import os
 qg = pipeline("e2e-qg")
 
@@ -10,20 +11,33 @@ class insi:
         self.qmaps=score_questions(questions)
         return self.qmaps
     
-    def get_questions(self,text):
+    def get_questions(self,text,csv=False):
         questions=qg(text)
-        qmaps=self.get_scores(questions)
-        picked_questions=[k for k,v in qmaps.items() if v>0.9]
-        return picked_questions
+        if csv:
+            qmaps=self.get_scores(questions)
+            picked_questions=[k for k,v in qmaps.items() if v>0.9]
+            return picked_questions
+        return questions
     
-    def get_results(self,text,csv_path,schema=None):
-        questions=self.get_questions(text)
-        agent=Agent(csv_path,schema)
+    def get_results(self,text,csv_path=None,schema=None):
+       
+        
         valmaps={}
-        for q in questions:
-            res=agent.query_db(q)
-            valmaps[q]=res
+        if csv_path:
+            questions=self.get_questions(text,csv=True)
+            agent=Agent(csv_path,schema)
+            
+            for q in questions:
+                res=agent.query_db(q)
+                valmaps[q]=res
+       
+        else:
+            questions=self.get_questions(text)
+            for q in questions:
+                valmaps[q]=qa(text,q)
+
         return valmaps
+            
         
     
         
